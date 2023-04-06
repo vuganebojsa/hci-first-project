7b04871fb02f6e3ebf48aba6640b4230bd7cf00e
Bogdan <bogdanjanosevic@gmail.com>
2023-04-06 18:39:50 +0200

    Added ngModel for input fields

--- news/src/app/app.module.ts
+++ news/src/app/app.module.ts
@@ -9,6 +9,7 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatNativeDateModule} from '@angular/material/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -24,6 +25,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatFormFieldModule,
     MatNativeDateModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
  ],
   providers: [],
   bootstrap: [AppComponent]
--- news/src/app/layout/search/search.component.ts
+++ news/src/app/layout/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -6,7 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+  chosenDateTime = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl(),
+  });
+  hasSelected: boolean = false
+  source: string = '';
+  category: string = '';
+  country: string = '';
+  title: string = '';
+  keyword: string = '';
+  startDate: string = '';
+  endDate: string = '';
+
+  ngOnInit(): void {
+    //Vrati sve opcije moguce
+  }
+
   search(){
+
+    this.hasSelected = true;
+    console.log(this.chosenDateTime.value.start);
+    console.log(this.chosenDateTime.value.end);
+    console.log(this.keyword);
     
   }
 
