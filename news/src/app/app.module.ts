a5dfb75b9824766f04c1e2a7620ed4907250b125
Nebojša Vuga <72966399+nebojsavuga@users.noreply.github.com>
2023-04-06 14:17:08 +0200

    Generated articles component

--- news/src/app/app.module.ts
+++ news/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { HomeComponent } from './layout/home/home.component';
+import { ArticlesComponent } from './layout/articles/articles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
