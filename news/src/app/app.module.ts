4f5ef9098070792f2698043d47566e077611464e
DusanBibin <96448632+DusanBibin@users.noreply.github.com>
2023-04-06 12:07:39 +0200

    Created article display component

--- news/src/app/app.module.ts
+++ news/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { ArticleDisplayComponent } from './layout/article-display/article-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ArticleDisplayComponent
   ],
   imports: [
     BrowserModule,
