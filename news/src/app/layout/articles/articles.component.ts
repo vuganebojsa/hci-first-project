55c223136a0629d7400a9838e3785807a9385390
Nebojša Vuga <72966399+nebojsavuga@users.noreply.github.com>
2023-04-06 14:36:22 +0200

    Generated article service

--- news/src/app/layout/articles/articles.component.ts
+++ news/src/app/layout/articles/articles.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from 'src/app/models/article';
 
 @Component({
   selector: 'app-articles',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./articles.component.css']
 })
 export class ArticlesComponent {
+  articles: Article[];
+  
+  constructor(){
 
+  }
 }
